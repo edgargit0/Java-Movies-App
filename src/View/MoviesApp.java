b732415db347a533bc4748b913504c251e10421b
Ed Pad <ed.padr@gmail.com>
2025-03-19 19:56:44 -0600

    add code to MoviesApp main class

--- src/View/MoviesApp.java
+++ src/View/MoviesApp.java
@@ -2,39 +2,72 @@ package View;
 
 import java.util.Scanner;
 
+import domain.Movie;
+import service.IMovieService;
+import service.MovieServiceImpl;
+
 public class MoviesApp {
     public static void main(String[] args) throws Exception {
-        
-        boolean exit=false;
+
+        boolean exit = false;
         Scanner console = new Scanner(System.in);
-        //TODO add implement interface
         
-        while(!exit){
-            try{
+        IMovieService movieService = new MovieServiceImpl();
+
+        while (!exit) {
+            try {
                 displayMenu();
-                //TODO add runOptions method
-    
-            }
-            catch(Exception e){
+                exit = runMenuOptions(console, movieService);
+            } catch (Exception e) {
                 System.out.println("An error has occur: " + e.getMessage());
             }
-            System.out.println();   //sout just to print an empty line
-        }//end while
+
+            System.out.println(); // sout just to print an empty line
+        } // end while
     }
 
-
-    //display menu method
-    private static void displayMenu(){
+    // display menu method
+    private static void displayMenu() {
         System.out.print("""
-            *** Menu ***
-            1. List Movies
-            2. Add Movie
-            3. Find Movie
-            4. Exit
-            Choose an option: 
-            """);
+                *** Menu ***
+                1. Add Movie
+                2. List Movies
+                3. Find Movie
+                4. Exit
+                Choose an option:
+                """);
     }
 
+    //runMenuOptions method
+    private static boolean runMenuOptions(Scanner console, IMovieService movieService) {
 
+        var option = Integer.parseInt(console.nextLine());
+        var exit = false;
+        
+        switch (option) {
+            case 1 -> {
+                System.out.println("Enter movie name");
+                var movieName = console.nextLine();
+                movieService.addMovie(new Movie(movieName));
+            }
+            case 2 -> {
+                movieService.listMovies();
+            }
+            case 3 -> {
+                System.out.println("Enter movie name to find");
+                var movieName = console.nextLine();
+                movieService.findMovie(new Movie(movieName));
+            }
+            case 4 -> {
+                System.out.println("finishing execution...");
+                exit=true;
+            }
+            default -> {
+                System.out.println("Invalid option " + option);
+                System.out.println("Please enter an option between 1 to 4");
+            }
+            return exit;
+        }
+    }
 
 }
